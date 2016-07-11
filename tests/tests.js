1a72a79b80f4e8e284e1909c2b683f7182d84427
Joel Lonbeck <joel@neutrinographics.com>
2016-07-11 16:57:26 -0700

    fixed bug in indexing resources

--- tests/tests.js
+++ tests/tests.js
@@ -7,22 +7,76 @@
         var client = new Door43Client('./out/library.sqlite', './out/res');
         
         describe('@Library Calls', function() {
-            it('returns a list of source languages', (done) => {
-                client.index.getSourceLanguages().then(function(result)  {
-                    assert(result.length > 0);
-                }).then(done, done);
+            describe('@Source Languages', function() {
+                it('returns a list of source languages', (done) => {
+                    client.index.getSourceLanguages().then(function(result)  {
+                        assert(result.length > 0);
+                    }).then(done, done);
+                });
+
+                it('returns a single source language', (done) => {
+                    client.index.getSourceLanguage('en').then(function(result) {
+                        assert(result.slug === 'en');
+                    }).then(done, done);
+                });
+
+                it('returns null for a missing source language', (done) => {
+                    client.index.getSourceLanguage('fake-lang').then(function(result) {
+                        assert(result === null);
+                    }).then(done, done);
+                });
             });
 
-            it('returns a single source language', (done) => {
-                client.index.getSourceLanguage('en').then(function(result) {
-                    assert(result.slug === 'en');
-                }).then(done, done);
+            describe('@Projects', function() {
+                it('returns an empty list of projects for a missing source language', (done) => {
+                    client.index.getProjects('fake-lang').then(function(result)  {
+                        assert(result.length === 0);
+                    }).then(done, done);
+                });
+
+                it('returns a list of projects in a source language', (done) => {
+                    client.index.getProjects('en').then(function(result)  {
+                        assert(result.length > 0);
+                    }).then(done, done);
+                });
+
+                it('returns a single project', (done) => {
+                    client.index.getProject('en', 'obs').then(function(result) {
+                        assert(result.slug === 'obs');
+                    }).then(done, done);
+                });
+
+                it('returns null for a missing project', (done) => {
+                    client.index.getProject('fake-lang', 'fake-project').then(function(result) {
+                        assert(result === null);
+                    }).then(done, done);
+                });
             });
 
-            it('returns null for a missing source language', (done) => {
-                client.index.getSourceLanguage('fake-lang').then(function(result) {
-                    assert(result === null);
-                }).then(done, done);
+            describe('@Resources', function() {
+                it('returns an empty list of resource for a fake project', (done) => {
+                    client.index.getResources('fake-lang', 'fake-project').then(function(result)  {
+                        assert(result.length === 0);
+                    }).then(done, done);
+                });
+
+                it('returns a list of resource in a project', (done) => {
+                    client.index.getResources('en', 'obs').then(function(result)  {
+                        assert(result.length > 0);
+                    }).then(done, done);
+                });
+
+                it('returns a single resource', (done) => {
+                    client.index.getResource('en', 'obs', 'obs').then(function(result) {
+                        assert(result.slug === 'obs');
+                    }).then(done, done);
+                });
+
+                it('returns null for a missing resource', (done) => {
+                    client.index.getResource('fake-lang', 'fake-project', 'fake-resource').then(function(result) {
+                        assert(result === null);
+                    }).then(done, done);
+                });
             });
         });
         
@@ -35,8 +89,8 @@
             });
 
             it('downloads the resource catalog from the api and indexes it', (done) => {
-                client.downloadCatalog('https://api.unfoldingword.org/ts/txt/2/catalog.json').then((result) => {
-                    // todo check if it worked
+                client.downloadCatalog('https://api.unfoldingword.org/ts/txt/2/catalog.json').then(() => {
+                    assert(client.index.getSourceLanguages().length > 0);
                 }).then(done, done);
             });
 
