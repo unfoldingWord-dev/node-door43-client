12330fdf7f39db24d71af52563a4611faf399f1d
Joel Lonbeck <joel@neutrinographics.com>
2016-07-12 11:29:53 -0700

    added resource container format to resource methods

--- tests/tests.js
+++ tests/tests.js
@@ -3,8 +3,12 @@
         Door43Client = require('../'),
         rimraf = require('rimraf');
 
+    const catalogUrl = 'https://api.unfoldingword.org/ts/txt/2/catalog.json'
+        , indexPath = './out/library.sqlite'
+        , resourceDir = './out/res';
+
     describe('@Door43', function () {
-        var client = new Door43Client('./out/library.sqlite', './out/res');
+        var client = new Door43Client(indexPath, resourceDir);
         
         describe('@Library Calls', function() {
             describe('@Source Languages', function() {
@@ -63,12 +67,14 @@
                 it('returns a list of resource in a project', (done) => {
                     client.index.getResources('en', 'obs').then(function(result)  {
                         assert(result.length > 0);
+                        assert(result[0].container_format !== null);
                     }).then(done, done);
                 });
 
                 it('returns a single resource', (done) => {
                     client.index.getResource('en', 'obs', 'obs').then(function(result) {
                         assert(result.slug === 'obs');
+                        assert(result.container_format !== null);
                     }).then(done, done);
                 });
 
@@ -85,12 +91,33 @@
 
             before((done) => {
                 rimraf.sync('./out');
+                // TRICKY: we have to re-initialize to load the latest schema
+                client = new Door43Client(indexPath, resourceDir);
                 done();
             });
 
             it('downloads the resource catalog from the api and indexes it', (done) => {
-                client.downloadCatalog('https://api.unfoldingword.org/ts/txt/2/catalog.json').then(() => {
+                client.downloadCatalog(catalogUrl).then(() => {
                     assert(client.index.getSourceLanguages().length > 0);
+                }).then(done, done);
+            });
+
+            it('downloads the resource catalog from the api and updates the existing indexes without creating duplicates', (done) => {
+                var data = client.index.getSourceLanguages();
+                for(var lang of data) {
+                    lang.projects = client.index.getProjects(lang.slug);
+                    for(var proj of lang.projects) {
+                        proj.resources = client.index.getResources(lang.slug, proj.slug);
+                    }
+                }
+                client.downloadCatalog(catalogUrl).then(() => {
+                    assert(client.index.getSourceLanguages().length === data.length);
+                    for(var lang of data) {
+                        assert(client.index.getProjects(lang.slug).length === lang.projects.length);
+                        for(var proj of lang.projects) {
+                            assert(client.index.getResources(lang.slug, proj.slug).length === proj.resources.lenght);
+                        }
+                    }
                 }).then(done, done);
             });
         });
