4236153396ec3bf4f5cc0c587121112923625f9e
Joel Lonbeck <joel@neutrinographics.com>
2016-08-18 00:36:04 -0700

    added ability to skip already downloaded containers

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var utils = require('./lib/utils');
 var Door43Client = require('./');
 var readline = require('readline');
 
+const catalogUrl = 'https://api.unfoldingword.org/ts/txt/2/catalog.json';
+const indexPath = './out/library.sqlite';
+const resourceDir = './out/res';
+
 function writePercent(percent) {
     readline.cursorTo(process.stdout, 0);
     readline.clearLine(process.stdout, 0);
@@ -16,22 +20,17 @@ gulp.task('clean', function (done) {
     rimraf.sync('./out');
     done();
 });
+
 gulp.task('index', function (done) {
-    const catalogUrl = 'https://api.unfoldingword.org/ts/txt/2/catalog.json'
-        , indexPath = './out/library.sqlite'
-        , resourceDir = './out/res';
     var client = new Door43Client(indexPath, resourceDir);
     client.updateIndex(catalogUrl, function(total, completed) {
         var percent = Math.round(10 * (100 * completed) / total) / 10;
         writePercent(percent);
     }).then(done, done);
 });
-gulp.task('download', function (done) {
-    const indexPath = './out/library.sqlite'
-        , resourceDir = './out/res';
-    rimraf.sync(resourceDir);
-    var client = new Door43Client(indexPath, resourceDir);
 
+gulp.task('download', function (done) {
+    var client = new Door43Client(indexPath, resourceDir);
     client.index.getSourceLanguages()
         .then(function(languages) {
             var list = [];
@@ -71,7 +70,16 @@ gulp.task('download', function (done) {
             }
             console.log('Downloading ' + list.length + ' items...');
             return utils.chain(client.downloadResourceContainer, function(err, data) {
-                console.log(err, 'while downloading', data);
+
+                if(err.message === 'Resource container already exists') {
+                    readline.cursorTo(process.stdout, 0);
+                    readline.clearLine(process.stdout, 0);
+                    console.log('Skipping', data);
+                } else {
+                    readline.cursorTo(process.stdout, 0);
+                    readline.clearLine(process.stdout, 0);
+                    console.log(err, 'while downloading', data);
+                }
                 return false;
             }, {compact: true, onProgress: function(total, completed) {
                 var percent = Math.round(10 * (100 * completed) / total) / 10;
