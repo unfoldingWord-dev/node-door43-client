96bb3b1b9e82d13b65ff3c11547382936d8071a7
Joel Lonbeck <joel@neutrinographics.com>
2016-10-26 17:10:24 -0700

    fixed import to support adding project categories

--- mocha_tests/import-test.js
+++ mocha_tests/import-test.js
@@ -3,7 +3,6 @@
 var rimraf = require('rimraf');
 var mkdirp = require('mkdirp');
 var assert = require('assert');
-var fileUtils = require('../lib/utils/files');
 var ncp = require('ncp').ncp;
 describe('Import', function() {
     let client;
@@ -19,7 +18,7 @@ describe('Import', function() {
     describe('import container', function() {
         this.timeout(10000);
 
-        it('should import an open resource container successfully', function() {
+        it('should import a resource container', function() {
             return client.importResourceContainer('mocha_tests/zzz_php_cdw')
                 .then(function(container) {
                     assert.equal(container.path, 'mocha_tests/out/containers/zzz_php_cdw');
@@ -28,6 +27,31 @@ describe('Import', function() {
                 })
                 .then(function(container) {
                     assert.equal(container.path, 'mocha_tests/out/containers/zzz_php_cdw');
+                });
+        });
+
+        it('should import a new resource container', function() {
+            return client.importResourceContainer('mocha_tests/en_tit-new_ulb')
+                .then(function(container) {
+                    assert.equal(container.path, 'mocha_tests/out/containers/en_tit-new_ulb');
+                    // attempt to open normally
+                    return client.openResourceContainer('en', 'tit-new', 'ulb');
+                })
+                .then(function(container) {
+                    assert.equal(container.path, 'mocha_tests/out/containers/en_tit-new_ulb');
+
+                    // make sure the indexed project is accessible
+                    let project = client.indexSync.getProject('en', 'tit-new');
+                    assert.ok(project != null);
+
+                    let entries = client.indexSync.getProjectCategories(0, "en", null);
+                    assert.equal(entries.length, 1);
+                    assert.equal(entries[0].slug, 'bible-nt');
+                    assert.equal(entries[0].type, 'category');
+                    let subEntries = client.indexSync.getProjectCategories(entries[0].id, "en", null);
+                    assert.equal(subEntries.length, 1);
+                    assert.equal(subEntries[0].slug, 'tit-new');
+                    assert.equal(subEntries[0].type, 'project');
                 });
         });
     });
